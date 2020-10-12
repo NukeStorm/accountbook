282c5317ecfc9a6c4c5776dc7c725a7f04d10c81
NukeStorm <nsdm821@gmail.com>
2020-10-13 06:32:01 +0900

    [feautre] 여러 component에서 사용될 함수 모음

---
+++ public/javascripts/lib/utils.js
@@ -0,0 +1,19 @@
+/* eslint-disable import/prefer-default-export */
+// 숫자를 미국식 표기법(3자리마다 , 추가) 문자열로 변환
+export function convertNumberFormat(number) {
+  return Number(number).toLocaleString('en');
+}
+
+// date undefined , null이면 현재 달 문자열 반환
+// date객체를 YYYY-MM형식 문자열로 변환
+export function getMonthStr(date) {
+  let now = null;
+  if (date) now = new Date(date);
+  else now = new Date();
+  const currentMonthStr = [now.getFullYear(), `0${now.getMonth() + 1}`.slice(-2)].join('-');
+  return currentMonthStr;
+}
+// RGB code생성하는 함수
+export function toRGBCode(red = 255, blue = 255, green = 255) {
+  return `#${red.toString(16)}${blue.toString(16)}${green.toString(16)}`;
+}
