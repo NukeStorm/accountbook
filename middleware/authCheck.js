790749ff96338e5948a116196e6a999d76614943
NukeStorm <nsdm821@gmail.com>
2020-10-16 11:37:23 +0900

    [feature/be] jwt 회원인증 토큰 체킹하는 미들웨어 구현 로그인한 회원에 한해서만 api호출 가능하도록 체크하는 미들웨어

---
+++ middleware/authCheck.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config({ path: '.env' });
+
+const authChecker = async (req, res, next) => {
+  if (!req.headers['x-auth-token']) {
+    console.log('no header');
+    return res.status(419).json({
+      code: 419,
+      message: '유효하지 않은 토큰',
+    });
+  }
+  const token = req.headers['x-auth-token'];
+  try {
+    const decodeObj = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(decodeObj);
+    res.locals.userid = decodeObj.userid;
+    next();
+  } catch (e) {
+    if (e.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code: 401,
+        message: '토큰 만료',
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰',
+    });
+  }
+};
+
+module.exports = authChecker;
