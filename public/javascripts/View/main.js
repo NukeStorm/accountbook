28e10a4f9fdae2b8eea113ae4a2035a784f6739c
NukeStorm <nsdm821@gmail.com>
2020-10-16 11:46:01 +0900

    [feature/fe]nabvar 컴포넌트 추가

--- public/javascripts/View/main.js
+++ public/javascripts/View/main.js
@@ -8,18 +8,18 @@ import ContentAddForm from '../Components/ContentAddForm.js';
 import CategoryFilter from '../Components/CategoryFilter.js';
 import HistoryList from '../Components/HistoryList.js';
 import MenuBar from '../Components/MenuBar.js';
-
+import NavBar from '../Components/NavBar.js';
 import View from '../lib/View.js';
 
 class MainView extends View {
   constructor() {
-    const componentList = [new MonthSelector('#monthSelector'), new MenuBar('#menu-bar'), new ContentAddForm('#content-add-form'), new CategoryFilter('#category-type-filter'), new HistoryList('#history-list')];
+    const componentList = [new MonthSelector('#monthSelector'), new NavBar('#nav-bar'), new MenuBar('#menu-bar'), new ContentAddForm('#content-add-form'), new CategoryFilter('#category-type-filter'), new HistoryList('#history-list')];
     const appState = new AccountHistoryState();
     super(componentList, appState);
   }
 
   createHtml() {
-    const viewHtmlStr = `<nav>가계부</nav>
+    const viewHtmlStr = `<nav id="nav-bar">가계부</nav>
    <div class="month" id="monthSelector"></div>
    <div class="menu-bar" id="menu-bar"> </div>
    <div class="content-add-form" id="content-add-form"></div>
