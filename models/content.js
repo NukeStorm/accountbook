45e27fad36a3bc937932dce5a1167c9c88edb4e9
Dongmin Shin <nsdm821@gmail.com>
2020-10-16 17:07:00 +0900

    [bugfix]sequelize Content Table 컬럼수정  Datetime->Date로

--- models/content.js
+++ models/content.js
@@ -12,7 +12,7 @@ class Content extends Sequelize.Model {
           autoIncrement: true,
         },
         date: {
-          type: Sequelize.DATE,
+          type: Sequelize.DATEONLY,
           allowNull: false,
           unique: false,
         },
