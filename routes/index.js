9022a4da376240175d16ca526690c7e92f099024
NukeStorm <nsdm821@gmail.com>
2020-10-16 11:45:09 +0900

    [feature/be] index 라우터 필요없는 라우팅 삭제

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,5 @@ const User = require('../models/user');
 router.get('/', (req, res, next) => {
   res.redirect('/main.html');
 });
-router.get('/calendar', (req, res, next) => {
-  res.redirect('/calendar.html');
-});
-router.get('/statistics', (req, res, next) => {
-  res.redirect('/statistics.html');
-});
+
 module.exports = router;
