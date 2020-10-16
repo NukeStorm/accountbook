581d352c99e243f4ca392652d1a35f69a9796615
NukeStorm <nsdm821@gmail.com>
2020-10-16 11:37:46 +0900

    [feature/be] payment table 추가

--- models/content.js
+++ models/content.js
@@ -21,7 +21,11 @@ class Content extends Sequelize.Model {
           allowNull: false,
           unique: false,
         },
-
+        payment: {
+          type: Sequelize.STRING(45),
+          allowNull: true,
+          unique: false,
+        },
         amount: {
           type: Sequelize.INTEGER,
           allowNull: false,
@@ -45,6 +49,7 @@ class Content extends Sequelize.Model {
   static associate(db) {
     db.Content.belongsTo(db.User, { foreignKey: 'userid', targetKey: 'userid' });
     db.Content.belongsTo(db.Category, { foreignKey: 'category', targetKey: 'cid' });
+    db.Content.belongsTo(db.Payment, { foreignKey: 'payment', targetKey: 'pid' });
   }
 }
 module.exports = Content;
